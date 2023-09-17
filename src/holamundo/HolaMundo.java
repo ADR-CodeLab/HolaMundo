85b3dc618e4956d85dbded8b9ca69877e38ac3d1
garcialopez <joseadrian_g97@hotmail.com>
2023-09-17 17:54:06 -0600

    Plantilla
    Esta es la plantilla del proyecto.

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,11 @@
+package holamundo;
+
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+
+        // Escribe tu código aquí
+        
+    } // Cierra metodo main
+    
+} // Cierra clase
