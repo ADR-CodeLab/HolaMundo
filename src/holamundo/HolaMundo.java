eaa9d9aee106133e240670f4afcc66940a0f0ea1
AdrianGL <52833089+garcialopez@users.noreply.github.com>
2023-09-17 18:19:37 -0600

    Update HolaMundo.java

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
     
     public static void main(String[] args) {
 
-        // Escribe tu código aquí
+        // Escribe tu codigo aqui
         
     } // Cierra metodo main
     
